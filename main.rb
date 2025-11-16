71f2d4e9ba56036aec48461bf8360f7502cffe06
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-11-16 16:02:56 +1100

    set up base files. gameplay described.

---
+++ main.rb
@@ -0,0 +1,14 @@
+# mastermind game gets started.
+# computer selects a secret code (made up of four pegs each randomly selected to be one of six colors).
+# player puts in a four peg code to test and the computer checks it against the secret code.
+# computer shows with red pegs the number of correct color pegs in the correct place.
+# computer shows with white pegs the number of correct color peg in the wrong place.
+# player gets a total of 12 guesses repeating the past three steps.
+# if a correct guess, player wins
+# else, after 12 guesses computer wins.
+
+# Objects:
+#   - computer that selects secret code
+#   - player? that handles player input?
+#   - board that remembers the current board state and checks for correct inputs.
+#   - board also checks if the code is guessed. (check for win.)
